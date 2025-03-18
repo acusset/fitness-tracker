6c6b566669416763d04148f20e37c8dc2762a5db
Antoine Cusset <acusset@palo-it.com>
2025-03-18 23:38:51 +0800

    chore(eslint): update ESLint configuration and add React plugin support 📦

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,8 +6,15 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
+  { settings: { react: { version: "19.0" } } },
+  { ignores: ["node_modules", ".next", "build", "dist", "public"] },
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
